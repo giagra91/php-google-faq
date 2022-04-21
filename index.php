b2dd8a6ce339a059e9bb252699fdb2769cba3eed
giagra91 <giagra91@gmail.com>
2022-04-21 16:07:46 +0200

    completamento esercizio

--- index.php
+++ index.php
@@ -80,8 +80,13 @@
                 <img class="my-img" src="https://camo.githubusercontent.com/06b39fc6b7e1ab60119bb11626220535f002634fe5e4bada9362ac26978ab6e1/68747470733a2f2f7261772e6769746875622e636f6d2f656c61646e6176612f6d6174657269616c2d6c65747465722d69636f6e732f6d61737465722f646973742f706e672f522e706e67" alt="">
             </div>
         </div>
-        <div class="bottom-header">
-            <h3>prova bottom</h3>
+        <div class="bottom-header flex">
+            <h3>Introduzione</h3>
+            <h3>Norme sulla privacy</h3>
+            <h3>Termini di Serivizio</h3>
+            <h3>Tecnologie</h3>
+            <h3>Domande Frequenti</h3>
+
         </div>
     </header>
     <main>
